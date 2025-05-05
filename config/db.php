1ecda34b1fb637607ea3fa39e37d0435546cb38d
Fatme <fatme34saeed@gmail.com>
2025-05-05 10:01:15 +0300

    Refactor db.php to use mysqli for both local and online environments

--- config/db.php
+++ config/db.php
@@ -33,20 +33,33 @@ if ($_SERVER["HTTP_HOST"] === "localhost") {
     $fields = parse_url($uri);
 
     // build the DSN including SSL settings
-    $conn = "mysql:";
-    $conn .= "host=" . $fields["host"];
-    $conn .= ";port=" . $fields["port"];;
-    $conn .= ";dbname=".$_ENV["DBNAME"];
-    $conn .= $_ENV["SSL_MODE"];
+    // $conn = "mysql:";
+    // $conn .= "host=" . $fields["host"];
+    // $conn .= ";port=" . $fields["port"];;
+    // $conn .= ";dbname=".$_ENV["DBNAME"];
+    // $conn .= $_ENV["SSL_MODE"];
+    $hostname = $fields["host"];
+    $username = $fields["user"];
+    $password = $fields["pass"];
+    $dbname = ltrim($fields["path"], '/');
+    $port = $fields["port"];
 
 
     try {
-        $db = new PDO($conn, $fields["user"], $fields["pass"]);
+        // $db = new PDO($conn, $fields["user"], $fields["pass"]);
+        $conn = new mysqli($hostname, $username, $password, $dbname, $port);
 
         //Testing Connection
         // $stmt = $db->query("SELECT VERSION()");
         // print($stmt->fetch()[0]);
-        echo "✅ Online DB connected successfully!";
+        // echo "✅ Online DB connected successfully!";
+
+        if ($conn->connect_error) {
+            throw new Exception("Connection failed: " . $conn->connect_error);
+        }else {
+            echo "✅ Online MySQL connected successfully!";
+        }
+
     } catch (Exception $e) {
         die("Online DB Connection Error: " . $e->getMessage());
     }
